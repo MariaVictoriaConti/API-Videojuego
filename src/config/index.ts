b61a21cf6ede13f644a3bb7b5c5397c312e20b83
sofisachetti <sachettisofia@gmail.com>
2024-11-08 17:43:58 -0300

    Agrego avances en models y controllers

---
+++ src/config/index.ts
@@ -0,0 +1,8 @@
+// ESQUEMA GENERAL DEL JUEGO
+// Bienvenida y explicacion
+// 1. Creacion del personaje: 
+// 2. Asignar mision al personaje creado
+// Debe poder elegir varias misiones
+// 3. Iniciar juego en la mision elegida.
+// Desarrolo de la mision, en donde entran los enemigos (warrior y mage), batallas y eventos sorpresa
+// 4. Completa la mision (con exito) o game over => elegir salir o iniciar de nuevo
