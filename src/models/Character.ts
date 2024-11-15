8879d8ca49220bff1d4a24311d177f9ed71bf508
sofisachetti <sachettisofia@gmail.com>
2024-11-15 17:10:09 -0300

    Edicion en Character, falta solo revisar setter de inventory

--- src/models/Character.ts
+++ src/models/Character.ts
@@ -1,33 +1,30 @@
-// faltan setters
-import { MissionType, Mission } from "./Mission";
-
-//Agrego poderes para el inventario
+//Enum de poderes que pude elegir el personaje
 export enum PODERES {
     SUPERSALTO = "SUPERSALTO",
     SUPERVELOCIDAD = "SUPERVELOCIDAD",
     PIZZABOOMERANG = "PIZZABOOMERANG",
 }
 
-//En vez de heredar use la clase mission directamente, como q la instancie dentro de un atributo
+//Creación de la clase Character
 export class Character{
     private _name: string;
     private _level: number;
     private _health: number;
     private _experience: number;
     public _inventory: string[];
-     public _listaMisiones: string[];
+    public _listaMisiones: string[];
 
-
+// Para el constructor lo único que se pide es el nombre. Level, health y experience se ponen ya de forma predeterminada.
     constructor(name: string) {
-
         this._name = name;
         this._level = 1;
         this._health = 100;
         this._experience = 0;
         this._inventory = [];
-         this._listaMisiones = []; 
+        this._listaMisiones = []; 
     }
 
+// Metodos getter
     public get name(): string {
         return this._name;
     }
@@ -47,69 +44,66 @@ export class Character{
     public get inventory(): string[] {
         return this._inventory;
     }
-  
 
+// Métodos setter
     public set name(value:string){
-          this._name = value;
+        this._name = value;
     }
+
     public set level(value:number){
-        this._level = value;
-  }
-  public set health(value:number){
-    this._health = value;
-}
-public set experience(value:number){
-    this._experience = value;
-}
+        if (value > 0) {
+            this._level = value;
+        }
+    }
 
-public set inventory(value:string[]){
-    this._inventory = [];
-}
-public set listaMisiones(value:string){
-    this.name = value;
-}
+    public set health(value:number){
+        if (value > 0) {
+            this._health = value;
+        }
+    }
 
-public atacarContrincante():void{
-    console.log(`${this.name} ataca..`);
-    this._health -= 20
-    if(this.health<=0){
-        console.log(this.name + "GAME OVER");        
-    }   else{
-        console.log(this._name +" pierde 20 de salud por atacar. Su salud actual es: " + this._health);        
-    } 
-}
-public recibirAtaqueSinDefensa():void{
-    if(this.health <= 0){            
-        console.log(`${this._name} has sido derrotado!`); 
-        console.log(this.name + ": GAME OVER");
-        
+    public set experience(value:number){
+        if (value > 0) {
+            this._experience = value;
+        }
+    }
 
-    }else if(this.health >= 29){
-        this.health -= 30;        
-        console.log(`${this.name} es atacado desprevenidamente.. y su salud disminuye a ${this.health}..`);
+    public set inventory(value:string[]){ // REVISAR 
+        this._inventory = [];
+    }
+
+    public set listaMisiones(value:string){
+        this.name = value;
+    }
+
+// Metodo atacarContrincante: con cada ataque que realiza, pierde puntos de health.
+    public atacarContrincante(): void {
+        console.log(`${this.name} ataca a su oponente...`);
+        this._health -= 20
+        if(this.health<=0){
+            console.log(this.name + " GAME OVER");        
+        } else {
+            console.log(this._name + " pierde 20 de salud por atacar. Su salud actual es: " + this._health);        
+        } 
+    }
+
+// Metodo recibirAtaqueSinDefensa, cuando Character es atacado desprevenido. Resta más puntos de health.
+    public recibirAtaqueSinDefensa(): void {
+        if (this.health <= 0) {            
+            console.log(`${this._name} has sido derrotado!`); 
+            console.log(this.name + ": GAME OVER");
+        } else if (this.health >= 29) {
+            this.health -= 30;        
+            console.log(`${this.name} es atacado desprevenidamente.. y su salud disminuye a ${this.health}..`);
+        }
+    }
+
+// Metodo recibirAtaqueConDefensa, cuando Character se defiende de los ataques de su oponente. 
+    public recibirAtaqueConDefensa():void{
+        if(this._health>=0){
+            console.log(`${this.name} es atacado pero se defiende y no afecta su salud.`);
+        }else{
+            console.log("game over");
+        }
     }
 }
-public recibirAtaqueConDefensa():void{
-    if(this._health>=0){
-        console.log(`${this.name} es atacado pero se defiende y no afecta su salud.`);
-    }else{
-        console.log("game over");
-        
-    }
-}
-}
-
-
-//const personaje1 = new Character("coco")
-//  console.log(personaje1); 
-// personaje1.asignarMision(MissionType.Main);
-// personaje1.asignarMision(MissionType.Side)
-// console.log(personaje1.listarMisiones)
-// personaje1.inventory = PODERES.PIZZABOOMERANG
-// console.log(personaje1.listarMisiones)
-// console.log(personaje1);
-
-// personaje1.inventory = PODERES.PIZZABOOMERANG
-
-
-
